aad139f16f11cb0cf66e93a189254e56891d73da
Sean Frankruyter <s.frankruyter@gmail.com>
2024-12-01 11:11:36 -0500

    Implement part2

--- dec-1-2024/src/main.rs
+++ dec-1-2024/src/main.rs
@@ -1,7 +1,8 @@
 use std::fs;
 
 fn main() {
-    part1();
+    // part1();
+    part2();
 }
 
 fn part1() {
@@ -39,4 +40,45 @@ fn part1() {
 
     let deltas_sum: u32 = deltas.iter().sum();
     println!("Sum of deltas: {}", deltas_sum);
+}
+
+fn part2() {
+    // let file_path = "src/exampleInput.txt";
+    let file_path = "src/input.txt";
+    let contents = fs::read_to_string(file_path)
+        .expect("Something went wrong reading the file");
+
+    let file_lines: Vec<_> = contents.lines().collect();
+
+    let mut left_numbers: Vec<u32> = Vec::new();
+    let mut right_numbers: Vec<u32> = Vec::new();
+    let mut similarity_scores: Vec<u32> = Vec::new();
+
+    for line in &file_lines {
+        let parts: Vec<&str> = line.split_whitespace().collect();
+
+        left_numbers.push(parts[0].parse::<u32>().unwrap());
+        right_numbers.push(parts[1].parse::<u32>().unwrap());
+    }
+
+    // remove duplicates from the left numbers
+    // left_numbers.sort();
+
+    for (i, &left) in left_numbers.iter().enumerate() {
+        let mut count: u32 = 0;
+
+        for (j, &right) in right_numbers.iter().enumerate() {
+            if left == right {
+                count += 1;
+            }
+        }
+
+        let score: u32 = left * count;
+
+        similarity_scores.push(score);
+
+        println!("Left: {}, Count: {},  Score: {}", left, count, score);
+        
+    }
+    println!("Sum of similarity scores: {}", similarity_scores.iter().sum::<u32>());
 }
