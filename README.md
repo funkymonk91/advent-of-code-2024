1a90413ae5296e6e28c3c28127ba2866251b4537
Sean Frankruyter <s.frankruyter@gmail.com>
2024-12-03 17:48:28 -0500

    init day 3

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 |----------------------------------------|--------|--------|
 | Day 1: Historian Hysteria              |   ✅   |   ✅   |
 | Day 2: Red-Nosed Reports               |   ✅   |   ✅   |
+| Day 3: Mull It Over                    |        |        |
 
---
+++ dec-3-2024/src/main.rs
@@ -0,0 +1,25 @@
+use common::time;
+use std::fs;
+
+fn main() {
+    println!("Mull It Over");
+    println!("------------------");
+
+    let input = fs::read_to_string("src/input.txt")
+        .expect("Something went wrong reading the file");
+
+    // let (duration, result) = time(|| part1(&input));
+    // println!("Part 1 result: {} (computed in {:?})", result, duration);
+
+    // let (duration, result) = time(|| part2(&input));
+    // println!("Part 2 result: {} (computed in {:?})", result, duration);
+    // println!();
+}
+
+fn part1(input: &String) -> u32 {
+    // 
+}
+
+fn part2(input: &String) -> u32 {   
+    // 
+}
