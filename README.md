623b935006d781ca98518efe92f4ba35693d4e8b
Sean Frankruyter <s.frankruyter@gmail.com>
2024-12-03 18:14:14 -0500

    part 1 complete

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 |----------------------------------------|--------|--------|
 | Day 1: Historian Hysteria              |   ✅   |   ✅   |
 | Day 2: Red-Nosed Reports               |   ✅   |   ✅   |
-| Day 3: Mull It Over                    |        |        |
+| Day 3: Mull It Over                    |   ✅   |        |
 
--- dec-3-2024/src/main.rs
+++ dec-3-2024/src/main.rs
@@ -1,5 +1,6 @@
 use common::time;
 use std::fs;
+use regex::Regex;
 
 fn main() {
     println!("Mull It Over");
@@ -8,8 +9,8 @@ fn main() {
     let input = fs::read_to_string("src/input.txt")
         .expect("Something went wrong reading the file");
 
-    // let (duration, result) = time(|| part1(&input));
-    // println!("Part 1 result: {} (computed in {:?})", result, duration);
+    let (duration, result) = time(|| part1(&input));
+    println!("Part 1 result: {} (computed in {:?})", result, duration);
 
     // let (duration, result) = time(|| part2(&input));
     // println!("Part 2 result: {} (computed in {:?})", result, duration);
@@ -17,9 +18,21 @@ fn main() {
 }
 
 fn part1(input: &String) -> u32 {
-    // 
+    let re = Regex::new(r"mul\(\d{1,3},\d{1,3}\)").unwrap();
+    re.captures_iter(input)
+        .map(|caps| {
+            // trim the "mul(" and ")"
+            let mul = caps.get(0).unwrap().as_str();
+            let mul = &mul[4..mul.len()-1];
+
+            let mut nums = mul.split(",").map(|n| n.parse::<u32>().unwrap());
+
+            let [a, b] = [nums.next().unwrap(), nums.next().unwrap()];
+            a * b
+        })
+        .sum()
 }
 
-fn part2(input: &String) -> u32 {   
-    // 
-}
+// fn part2(input: &String) -> u32 {   
+//     0
+// }
